296067af75b482dc181fac92282c19ad2b19f9e4
Aznavour <vic007tor@gmail.com>
2020-03-05 16:37:47 -0500

    finalisation  php grid & css grid

--- category-evenement.php
+++ category-evenement.php
@@ -20,48 +20,47 @@ do_action( 'astral_top_banner' );
         <div class="container">
             <?php
                 
-                wp_reset_postdata();
-                $args2 = array(
-                "category_name"=>"evenement",
-                "order"=>"ASC",
-                "orderBy"=>"date");
-               
-                /*The 2nd Query (without global var) */
-                $query2 = new WP_Query( $args2 );
-                 
-               
-               $category = get_the_category($query2->post->ID);
-               
-               echo "<h1>".category_description($category[0])."</h1>";
-               echo '<div class="oGrid" style="background-color:white;">';
-                // The 2nd Loop
-                $iPostId = $query2->post->ID;
                 
-                while ( $query2->have_posts()) {
+               
+               
+                echo "<h1>".category_description()."</h1>";
+               echo '<div class="oGrid">';
+                // The 2nd Loop
+
+                while (have_posts()) {
+                   the_post();
+
+			        get_template_part( 'template-parts/content', 'evenement' );
+                    $oMois = (int)get_the_date("m");
+                    $oJour = (int)get_the_date("j");
+
                     
-                    $oMois = get_the_date("m");
-                    var_dump($query2->post->post_date);
-                    switch($oMois%10){
-                        case 0 :
-                            $query2->the_post();
-                            echo '<div style="background-color:lightgrey; padding:1%;" class="Octobre">';
-                            echo '<h4>' . get_the_title() . ' - ' . get_the_date() . '</h4>';
-                            echo '</div>';
-                        break;
+                    
+                    switch($oMois%9){
+                    case 0:echo "<div class='septembre' style='grid-area:".$oJour."/".($oMois%9 + 1)."/".($oJour+1)."/".($oMois%9 + 1).";' id=".get_the_ID().">";
+                    echo "<p>" . get_the_date("j") ." - ".get_the_date("m"). " - ".get_the_date("y") ."</p>";
+                    echo "<p>".get_the_title()."</p>";
+                    echo "</div>";
+                    break;
 
-                        case 1 :
-                            $query2->the_post();
-                            echo '<div style="background-color:lightgrey; padding:1%;" class="Novembre">';
-                            echo '<h4>' . get_the_title() . ' - ' . get_the_date() . '</h4>';
-                            echo '</div>';
-                        break;
+                    case 1:
 
-                        case 2:
-                            $query2->the_post();
-                            echo '<div style="background-color:lightgrey; padding:1%;" class="Decembre">';
-                            echo '<h4>' . get_the_title() . ' - ' . get_the_date() . '</h4>';
-                            echo '</div>';
-                        break;
+                    echo "<div class='octobre' style='grid-area:".$oJour."/".($oMois%9 + 1)."/".($oJour+1)."/".($oMois%9 + 1).";' id=".get_the_ID().">";
+                    echo "<p>" . get_the_date("j") ." - ".get_the_date("m"). " - ".get_the_date("y") ."</p>";
+                    echo "<p>".get_the_title()."</p>";
+                    echo "</div>";
+                    break;
+                    
+                    
+
+                    case 2:
+                    echo "<div class='novembre' style='grid-area:".$oJour."/".($oMois%9 + 1)."/".($oJour+1)."/".($oMois%9 + 1).";' id=".get_the_ID().">";
+                    echo "<p>" . get_the_date("j") ." - ".get_the_date("m"). " - ".get_the_date("y") ."</p>";
+                    echo "<p>".get_the_title()."</p>";
+                    echo "</div>";
+                    break;
+                    }
+                    
                     }
                            
                             
@@ -69,7 +68,7 @@ do_action( 'astral_top_banner' );
                         
 
                         
-                    }
+                    
                     
                 
                  
